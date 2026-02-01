29eff1ecbaaa0cbcaceff4d07754d420892b5323
Fayez Ismail <fayezismail2025@gmail.com>
2026-02-01 15:25:41 -0500

    Update div.v

---
+++ alu/div.v
@@ -0,0 +1,16 @@
+// DIV operation
+// Signed division of A by B.
+// Quotient is placed in Z[31:0] (LO register)
+// Remainder is placed in Z[63:32] (HI register)
+//---...---//
+// Division by zero is safely handled
+
+module div(input [31:0] A, input [31:0] B, output [63:0] Z);
+  wire signed [31:0] a = A;
+  wire signed [31:0] b = B;
+
+  wire signed [31:0] q = (b == 0) ? 32'sd0 : (a / b);
+  wire signed [31:0] r = (b == 0) ? 32'sd0 : (a % b);
+
+  assign Z = {r, q};
+endmodule
