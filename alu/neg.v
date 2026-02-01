8ea4a2416d724e1d5e401482408161f631528219
Fayez Ismail <fayezismail2025@gmail.com>
2026-02-01 15:04:48 -0500

    Remove unnecessary comments in neg.v

--- alu/neg.v
+++ alu/neg.v
@@ -1,9 +1,10 @@
 // NEG operation
 // Computes the two's complement of A, Make A NEGative
 // B is unused but kept for a consistent ALU interface. (We can remove it, if causes problems and/or contradictions in the future)
-// Output is widened to 64 bits for the Z register.
+// Output is widened to 64 bits for the Z register
 
 module neg(input [31:0] A, input [31:0] B, output [63:0] Z);
   assign Z = {{32{1'b0}}, (-$signed(A))};
 endmodule
+
 
