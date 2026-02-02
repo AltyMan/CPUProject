9c1baef4a42e02203317405072b13f814115b201
Fayez Ismail <fayezismail2025@gmail.com>
2026-02-02 14:36:56 -0500

    Refactor ROL module to include rotation logic
    Updated ROL module to implement rotation logic directly instead of using a helper function.

--- alu/rol.v
+++ alu/rol.v
@@ -1,12 +1,21 @@
-// ROL (rotate left)
-// Rotates bits of A to the left by B[4:0] positions
-// Bits that fall off the left enter on the right
+`timescale 1ns/10ps
 
-module rol(input [31:0] A, input [31:0] B, output [63:0] Z);
-  
-  wire [4:0] sh = B[4:0];
-  wire [31:0] r;
-  helper h();
-  assign r = h.rol32(A, sh);
-  assign Z = {{32{1'b0}}, r};
+module rol(A, B, Z);
+
+   input  [31:0] A, B;
+   output [63:0] Z;
+
+   wire   [4:0]  sh;
+   wire   [31:0] r;
+
+   assign sh = B[4:0];
+
+   // Rotate left:
+   // (A << sh) moves bits left
+   // (A >> (32 - sh)) wraps the bits around
+   assign r = (A << sh) | (A >> (5'd32 - sh));
+
+   // Zero-extend to 64 bits (upper 32 are 0)
+   assign Z = {32'b0, r};
+
 endmodule
