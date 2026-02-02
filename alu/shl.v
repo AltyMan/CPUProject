3bd68d8f006ae3f2aaa794c984e1b7da7318af26
Fayez Ismail <fayezismail2025@gmail.com>
2026-02-02 14:38:16 -0500

    Update shl.v

--- alu/shl.v
+++ alu/shl.v
@@ -1,8 +1,15 @@
-// SHL (Shift Left Logical)
-// Shifts A left by the amount specified in B[4:0]
-// Only the lower 5 bits of B are used because shifting by more than 31 bits is basecally unnecessary for a 32-bit value.
+`timescale 1ns/10ps
 
-module shl(input [31:0] A, input [31:0] B, output [63:0] Z);
-  wire [4:0] sh = B[4:0];
-  assign Z = {{32{1'b0}}, (A << sh)};
+module shl(A, B, Z);
+
+   input  [31:0] A, B;
+   output [63:0] Z;
+
+   wire   [4:0] sh;
+   wire   [31:0] r;
+
+   assign sh = B[4:0];
+   assign r = A << sh;
+   assign Z = {32'b0, r};
+
 endmodule
