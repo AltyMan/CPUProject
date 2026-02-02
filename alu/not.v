8d3fe55c0ea6cc87ceaa4331646cb97bfb317718
Fayez Ismail <fayezismail2025@gmail.com>
2026-02-02 14:35:00 -0500

    Update not.v

--- alu/not.v
+++ alu/not.v
@@ -1,8 +1,12 @@
-// NOT operation
-// Bitwise NOT of A.
-// B is unused but kept for a consistent ALU interface. (We can remove it, if causes and contradictions in the future)
-// Output is 64-bit, result in the lower 32 bits
+`timescale 1ns/10ps
 
-module not_op(input [31:0] A, input [31:0] B, output [63:0] Z);
-  assign Z = {{32{1'b0}}, (~A)};
+module not(A, Result);
+
+   input  [31:0] A;
+   output [31:0] Result;
+
+   wire   [31:0] Result;
+
+   assign Result = ~A;
+
 endmodule
