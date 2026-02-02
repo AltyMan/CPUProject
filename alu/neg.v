e948539b8810d60fc64452b1b8053d0bbfb53e50
Fayez Ismail <fayezismail2025@gmail.com>
2026-02-02 14:34:11 -0500

    Update neg.v

--- alu/neg.v
+++ alu/neg.v
@@ -1,10 +1,14 @@
-// NEG operation
-// Computes the two's complement of A, Make A NEGative
-// B is unused but kept for a consistent ALU interface. (We can remove it, if causes problems and/or contradictions in the future)
-// Output is widened to 64 bits for the Z register
+`timescale 1ns/10ps
 
-module neg(input [31:0] A, input [31:0] B, output [63:0] Z);
-  assign Z = {{32{1'b0}}, (-$signed(A))};
+module neg(A, Result);
+
+   input  [31:0] A;
+   output [31:0] Result;
+
+   wire   [31:0] A_inv;
+   wire   [31:0] Result;
+
+   assign A_inv = ~A;
+   RCA NEGATE(A_inv, 32'b00000000000000000000000000000001, Result);
+
 endmodule
-
-
