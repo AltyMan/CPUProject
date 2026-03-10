12c613025713ac6106a5d0e38836376b1ac808a1
Fayez Ismail <fayezismail2025@gmail.com>
2026-03-10 09:36:42 -0400

    Enhance comments for decoder and bus logic
    Added comments to clarify the functionality of the decoder block and various bus checks.

--- core/CONFF.v
+++ core/CONFF.v
@@ -15,6 +15,8 @@ module CON_FF (
   wire c2_10;
   wire c2_11;
 
+  // This is the decoder block in the diagram.
+  // Decodes the c2 IR bits 
   assign c2_00 = (c2 == 2'b00);
   assign c2_01 = (c2 == 2'b01);
   assign c2_10 = (c2 == 2'b10);
@@ -26,22 +28,28 @@ module CON_FF (
   wire bus_is_negative;
 
   // = 0
+  // |bus == OR all bits together
   assign bus_is_zero    = ~|bus;    
 
   // != 0
+  //If any bit is 1 then the number is nonzero
   assign bus_is_nonzero =  |bus;   
   
   // >= 0
+  // Reverse all the bits
   assign bus_is_positive = ~bus[31];
   
   // < 0
+  // If the sign bit is 1 the number is negative
   assign bus_is_negative =  bus[31];            
 
-    // Full CON logic
+  // Full CON logic
+  // Now we connect the decoder outputs with the value checks
+  // The OR gate at the end 
   wire con_next;
   assign con_next = (c2_00 & bus_is_zero) | (c2_01 & bus_is_nonzero) | (c2_10 & bus_is_positive)| (c2_11 & bus_is_negative);
 
-    // CON flip-flop
+  // CON flip-flop
   always @(posedge clk or posedge reset) begin
       if (reset)
           CON <= 1'b0;
