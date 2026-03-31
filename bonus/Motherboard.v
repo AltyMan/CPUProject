1526f5891331a232778ae052ba85198f07a04bea
Amit Altman (21AA255) <amit.altman@queensu.ca>
2026-03-31 13:05:07 -0400

    New Motherboard with Debouncer for 50MHz

--- bonus/Motherboard.v
+++ bonus/Motherboard.v
@@ -1,48 +1,76 @@
 `timescale 1ns / 10ps
 
-module Motherboard#(parameter DIVISOR = 1)(
-	input wire CLOCK_50,
-	input wire [2:0] KEY, // key[0] = reset, key[1] = stop, key[2] = irq
+module Motherboard(
+    input wire CLOCK_50,
+    input wire [2:0] KEY,
     input wire [8:0] SW,
     output wire [5:5] LEDR,
     output wire [7:0] HEX0,
     output wire [7:0] HEX1
 );
-wire cpu_clock;
-ClockDivider #(.DIVISOR(DIVISOR)) clk_div(
-    .clk_in(CLOCK_50),
-    .reset(1'b0),
-    .clk_out(cpu_clock)
-);
+
+reg [19:0] reset_counter = 0;
+reg clean_reset = 0;
+always @(posedge CLOCK_50) begin
+    if (~KEY[0]) begin 
+        reset_counter <= 20'd1000000;
+        clean_reset <= 1'b1;
+    end else if (reset_counter > 0) begin
+        reset_counter <= reset_counter - 1'b1;
+        clean_reset <= 1'b1;
+    end else begin
+        clean_reset <= 1'b0;
+    end
+end
+
+reg [19:0] stop_counter = 0;
+reg clean_stop = 0;
+always @(posedge CLOCK_50) begin
+    if (~KEY[1]) begin
+        stop_counter <= 20'd1000000;
+        clean_stop <= 1'b1;
+    end else if (stop_counter > 0) begin
+        stop_counter <= stop_counter - 1'b1;
+        clean_stop <= 1'b1;
+    end else begin
+        clean_stop <= 1'b0;
+    end
+end
+
 reg irq_sync_1 = 0;
 reg irq_sync_2 = 0;
-always @(posedge cpu_clock) begin
+always @(posedge CLOCK_50) begin
     irq_sync_1 <= ~KEY[2];
-	 irq_sync_2 <= irq_sync_1;
+    irq_sync_2 <= irq_sync_1;
 end
 wire irq_pulse = irq_sync_1 & ~irq_sync_2;
+
 wire [31:0] InPortData;
+assign InPortData = {23'b0, SW[8:0]}; 
 wire [31:0] OutPortData;
-assign InPortData = {23'b0, SW[8:0]};
 wire cpu_run;
+
 DataPath dp(
-    .clock(cpu_clock),
-    .clear(~KEY[0]),
-    .stop(~KEY[1]),
+    .clock(CLOCK_50),
+    .clear(clean_reset),
+    .stop(clean_stop),
     .IRQ(irq_pulse),
     .InPortData(InPortData),
     .OutPortData(OutPortData),
     .run(cpu_run)
 );
+
 assign LEDR[5] = cpu_run;
+
 SevenSegDisplay hex0(
-    .clk(cpu_clock),
+    .clk(CLOCK_50),
     .data(OutPortData[3:0]),
     .seg(HEX0)
 );
 SevenSegDisplay hex1(
-    .clk(cpu_clock),
+    .clk(CLOCK_50),
     .data(OutPortData[7:4]),
     .seg(HEX1)
 );
+
 endmodule
