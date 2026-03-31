f9646a5e6ccc6263132f94b0eeb07ba47d890bc9
YehiaNoureldin <140462132+YehiaNoureldin@users.noreply.github.com>
2026-03-31 12:19:11 -0400

    Motherboard Interrupt FIx

--- bonus/Motherboard.v
+++ bonus/Motherboard.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 10ps
 
-module Motherboard#(parameter DIVISOR = 3)(
+module Motherboard#(parameter DIVISOR = 1)(
 	input wire CLOCK_50,
 	input wire [2:0] KEY, // key[0] = reset, key[1] = stop, key[2] = irq
     input wire [7:0] SW,
@@ -11,9 +11,16 @@ module Motherboard#(parameter DIVISOR = 3)(
 wire cpu_clock;
 ClockDivider #(.DIVISOR(DIVISOR)) clk_div(
     .clk_in(CLOCK_50),
-    .reset(~KEY[0]),
+    .reset(1'b0),
     .clk_out(cpu_clock)
 );
+reg irq_sync_1 = 0;
+reg irq_sync_2 = 0;
+always @(posedge cpu_clock) begin
+    irq_sync_1 <= ~KEY[2];
+	 irq_sync_2 <= irq_sync_1;
+end
+wire irq_pulse = irq_sync_1 & ~irq_sync_2;
 wire [31:0] InPortData;
 wire [31:0] OutPortData;
 assign InPortData = {24'b0, SW[7:0]};
@@ -22,7 +29,7 @@ DataPath dp(
     .clock(cpu_clock),
     .clear(~KEY[0]),
     .stop(~KEY[1]),
-    .IRQ(KEY[2]),
+    .IRQ(irq_pulse),
     .InPortData(InPortData),
     .OutPortData(OutPortData),
     .run(cpu_run)
